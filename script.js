4f5dbfe7aca405d52a6339f6c765e697f65be01b
morelmiles <morelmiles@gmail.com>
2020-03-24 09:23:07 +0300

    Added the js task

---
+++ script.js
@@ -0,0 +1,16 @@
+const name = "Luigi Morel";
+//A console log to display the name
+// console.log(name);
+
+const courses = ["Design", "Front-end", "Coding"];
+//A console log to display the courses
+// console.log(courses);
+
+//Since the number of courses are odd, I will find and display all odd numbers from 1-200 (inclusive)
+
+for (let i = 0; i <= 200; i++) {
+  if (i % 2 != 0) {
+    console.log(i);
+  }
+}
+a;
